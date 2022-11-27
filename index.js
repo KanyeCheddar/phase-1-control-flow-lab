bf00bc9128cf065acf577eb870cec971c4e0e5ce
Akeem Lawrence <akeemwlawrence@gmail.com>
2022-11-27 12:32:59 -0600

    solved first test

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let ride;
+
+  if(someValue <= 400) {
+    ride = 'This one is on me!';
+  } else if(someValue <= 2000) {
+    ride = 'That will be twenty bucks.';
+
+  } else if (someValue <= 2500) {
+    ride = 'I will gladly take your thirty bucks.'
+
+  } else if (someValue >= 2500) {
+    ride = 'No can do.';
+  } else {
+
+  }
+  return ride;
 }
 
 function ternaryCheckCity(){
